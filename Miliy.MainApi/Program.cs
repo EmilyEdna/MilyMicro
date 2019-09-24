779f1e09dc5ef643ed1b2e4065d261cd6a65ae05
Emily <847432003@qq.com>
2019-09-24 14:51:07 +0800

    Revert "替换项目为.net Core 3.0"
    This reverts commit aa5ffa42f331ee117069aac84c1b900b22fae521.

--- Miliy.MainApi/Program.cs
+++ Miliy.MainApi/Program.cs
@@ -5,8 +5,6 @@ using System.Linq;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Hosting.WindowsServices;
-using Microsoft.Extensions.Hosting;
-using XExten.XCore;
 
 namespace Miliy.MainApi
 {
@@ -14,32 +12,20 @@ namespace Miliy.MainApi
     {
         public static void Main(string[] args)
         {
-            if (!args.Contains("--Service:"))
-                CreateHostBuilder(args).Build().Run();
+            if (!args.Contains("--Service"))
+                CreateHostBuilder(args).UseStartup<Startup>().Build().Run();
             else
             {
-                args.ToEach<String>(item =>
-                {
-                    //open cmd with administrator and input SC CREATE [SERVERNAME] BINPATH="[PROGRAM_URL]\*.EXE" --Service:1024
-                    if (item.IsContainsIn("--Service:"))
-                    {
-                        int Port = Convert.ToInt32(item.Split(":")[1]);
-                        Startup.Port = Port + 1;
-                        CreateWebHostBuilder(args, Port);
-                    }
-                });
-            }
-        }
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
-        public static void CreateWebHostBuilder(string[] args, int Port) =>
-            WebHost.CreateDefaultBuilder(args).UseKestrel()
+                //open cmd with administrator and input SC CREATE [SERVERNAME] BINPATH="[PROGRAM_URL]\*.EXE" --Service
+                Console.Write("请输入端口号：");
+                int Port = Convert.ToInt32(Console.ReadLine());
+                Startup.Port = Port + 1;
+                CreateHostBuilder(args).UseKestrel()
                 .UseContentRoot(Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName))//加载当前目录
                 .UseUrls($"http://0.0.0.0:{Port}")//监听的IP和端口
                 .UseStartup<Startup>().Build().RunAsService();
+            }
+        }
+        public static IWebHostBuilder CreateHostBuilder(string[] args) => WebHost.CreateDefaultBuilder(args);
     }
 }
