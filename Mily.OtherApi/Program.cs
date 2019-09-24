a847e9fc726292205887d9c609a977953a2f0a14
Emily <847432003@qq.com>
2019-09-24 18:05:25 +0800

    更换到2.2

--- Mily.OtherApi/Program.cs
+++ Mily.OtherApi/Program.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Hosting.WindowsServices;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using XExten.XCore;
 
 namespace Mily.OtherApi
 {
@@ -16,18 +17,23 @@ namespace Mily.OtherApi
     {
         public static void Main(string[] args)
         {
-            if (!args.Contains("--Service"))
+            if (!args.Contains("--Service:"))
                 CreateHostBuilder(args).UseStartup<Startup>().Build().Run();
             else
             {
                 //open cmd with administrator and input SC CREATE [SERVERNAME] BINPATH="[PROGRAM_URL]\*.EXE" --Service
-                Console.Write("请输入端口号：");
-                int Port = Convert.ToInt32(Console.ReadLine());
-                Startup.Port = Port + 1;
-                CreateHostBuilder(args).UseKestrel()
-                .UseContentRoot(Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName))//加载当前目录
-                .UseUrls($"http://0.0.0.0:{Port}")//监听的IP和端口
-                .UseStartup<Startup>().Build().RunAsService();
+                args.ToEach<string>(item =>
+                {
+                    if (item.Trim().IsContainsIn("--Service:"))
+                    {
+                        int Port = Convert.ToInt32(item.Split(":")[1].Trim());
+                        Startup.Port = Port + 1;
+                        CreateHostBuilder(args).UseKestrel()
+                        .UseContentRoot(Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName))//加载当前目录
+                        .UseUrls($"http://0.0.0.0:{Port}")//监听的IP和端口
+                        .UseStartup<Startup>().Build().RunAsService();
+                    }
+                });
             }
         }
         public static IWebHostBuilder CreateHostBuilder(string[] args) => WebHost.CreateDefaultBuilder(args);
