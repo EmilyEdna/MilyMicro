6e7721b44cdc348e11d7020cc3d6149e3b896661
EmilyEdna <847432003@qq.com>
2019-11-24 10:31:44 +0800

    切换wpf

--- Mily.Forms/App.xaml.cs
+++ Mily.Forms/App.xaml.cs
@@ -1,9 +1,15 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
 
 namespace Mily.Forms
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    /// App.xaml 的交互逻辑
     /// </summary>
     public partial class App : Application
     {
