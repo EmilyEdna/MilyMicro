7c66ad2485bae320491b37b1b412cc77a7529cc9
EmilyEdna <847432003@qq.com>
2019-12-24 17:00:13 +0800

    完成服务中心的负载开发

--- Mily.Service/CenterApi/ServCenterApi.cs
+++ Mily.Service/CenterApi/ServCenterApi.cs
@@ -3,13 +3,11 @@ using BeetleX.FastHttpApi;
 using BeetleX.FastHttpApi.Data;
 using Mily.Service.CenterApi.CenterAop;
 using Mily.Service.CenterApi.ViewModel;
-using Mily.Service.CenterRpc.RpcSetting.Handler;
 using MongoDB.Driver;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
 using XExten.CacheFactory;
-using XExten.Common;
 using XExten.XCore;
 using System.Collections.Generic;
 
@@ -20,27 +18,70 @@ namespace Mily.Service.CenterApi
     [CenterFilter]
     public class ServCenterApi
     {
-        [Post]
-        [JsonDataConvert]
-        public void insert(IHttpContext Context)
-        {
-            ServerCondition Condition = Context.Data.Copy().FirstOrDefault().Value.ToJson().ToModel<ServerCondition>();
-            Caches.MongoDBCacheSet(Condition);
-        }
+        /// <summary>
+        /// 获取服务器
+        /// </summary>
+        /// <returns></returns>
         [Get]
         public async Task<Object> GetServer()
         {
-            return await Caches.MongoDBCacheGetAsync<ServerCondition>(t => true);
+            return await Caches.MongoDBCachesGetAsync<ServerCondition>(t => true);
         }
+        /// <summary>
+        /// 添加路由
+        /// </summary>
+        /// <param name="Context"></param>
+        /// <returns></returns>
         [Post]
         public async Task<String> InsertRoute(IHttpContext Context)
         {
             List<ServerCondition> Conditions = Context.Data.Copy().FirstOrDefault().Value.ToJson().ToModel<List<ServerCondition>>();
             Conditions.ForEach(Item =>
             {
-                Caches.MongoDbCacheUpdateAsync<ServerCondition>(t => t.ServiceName == Item.ServiceName, "Route",Item.Route);
+                Caches.MongoDbCacheUpdateAsync<ServerCondition>(t => t.ServiceName == Item.ServiceName, "Route", Item.Route);
             });
-            return await Task.FromResult("添加成功");
+            return await Task.FromResult("添加成功!");
+        }
+        /// <summary>
+        /// 启用HTTP负载
+        /// </summary>
+        /// <param name="Context"></param>
+        /// <returns></returns>
+        [Post]
+        public async Task<String> StarUseHttp(IHttpContext Context)
+        {
+            ServerCondition Conditions = Context.Data.Copy().FirstOrDefault().Value.ToJson().ToModel<ServerCondition>();
+            var Condition = Caches.MongoDBCachesGet<ServerCondition>(t => Conditions.ServiceName == t.ServiceName).Any(t => t.HttpPort.IsNullOrEmpty());
+            if (Condition)
+                return await Task.FromResult("请先添加HTTP端口号!");
+            else
+                Caches.MongoDbCacheUpdate<ServerCondition>(t => t.ServiceName == Conditions.ServiceName, "UseHttp", Conditions.UseHttp.ToString());
+            return await Task.FromResult(Conditions.UseHttp ? "启用成功!" : "禁用成功!");
+        }
+        /// <summary>
+        /// 添加HTTP服务
+        /// </summary>
+        /// <param name="Context"></param>
+        /// <returns></returns>
+        [Post]
+        public async Task<String> SaveHttpServer(IHttpContext Context)
+        {
+            ServerCondition Condition = Context.Data.Copy().FirstOrDefault().Value.ToJson().ToModel<ServerCondition>();
+            Caches.MongoDbCacheUpdate<ServerCondition>(t => t.Key == Condition.Key, "HttpPort", Condition.HttpPort);
+            return await Task.FromResult("保存成功!");
+        }
+        /// <summary>
+        /// 添加负载权重
+        /// </summary>
+        /// <param name="Context"></param>
+        /// <returns></returns>
+        [Post]
+        public async Task<String> SaveWeightRabin(IHttpContext Context)
+        {
+            ServerCondition Condition = Context.Data.Copy().FirstOrDefault().Value.ToJson().ToModel<ServerCondition>();
+            Caches.MongoDbCacheUpdate<ServerCondition>(t => t.Key == Condition.Key, "HttpWeight", Condition.HttpWeight.ToString());
+            Caches.MongoDbCacheUpdate<ServerCondition>(t => t.Key == Condition.Key, "TcpWeight", Condition.TcpWeight.ToString());
+            return await Task.FromResult("添加成功!");
         }
     }
 }
--- Mily.Service/CenterApi/ViewModel/ServerCondition.cs
+++ Mily.Service/CenterApi/ViewModel/ServerCondition.cs
@@ -6,15 +6,45 @@ namespace Mily.Service.CenterApi.ViewModel
 {
     public class ServerCondition:BaseCondition
     {
+        /// <summary>
+        /// Session Id
+        /// </summary>
         public int No { get; set; }
+        /// <summary>
+        /// 服务名称
+        /// </summary>
         public string ServiceName { get; set; }
+        /// <summary>
+        /// IP地址
+        /// </summary>
         public string Host { get; set; }
+        /// <summary>
+        /// TCP端口
+        /// </summary>
         public string TcpPort { get; set; }
+        /// <summary>
+        /// HTTP端口
+        /// </summary>
         public string HttpPort { get; set; }
+        /// <summary>
+        /// 路由
+        /// </summary>
         public string Route { get; set; }
         /// <summary>
-        /// 1 表示正常 0 表示错误
+        ///状态 1 表示正常 0 表示错误
         /// </summary>
         public int Stutas { get; set; }
+        /// <summary>
+        /// 是否启用HTTP
+        /// </summary>
+        public bool UseHttp { get; set; }
+        /// <summary>
+        /// TCP权重
+        /// </summary>
+        public int TcpWeight { get; set; }
+        /// <summary>
+        /// HTTP权重
+        /// </summary>
+        public int HttpWeight { get; set; }
     }
 }
