4c57a338cac214f863ff21b51baf10586aadc587
EmilyEdna <847432003@qq.com>
2020-02-03 15:20:22 +0800

    WPF view

---
+++ Mily.Forms/TreeViewTest/HeadToImageConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace Mily.Forms.TreeViewTest
+{
+    [ValueConversion(typeof(string), typeof(BitmapImage))]
+    public class HeadToImageConverter : IValueConverter
+    {
+        public static HeadToImageConverter Instance = new HeadToImageConverter();
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var path = (value as string);
+            if (path == null)
+                return null;
+            var name = Treeview.GetFileOrFolderName(path);
+            var Icon = "Image/10.png";
+            if (string.IsNullOrEmpty(name))
+            {
+                Icon = "Image/6.png";
+            }
+            else if (new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
+            {
+                Icon = "Image/2.png";
+            }
+            var url = new Uri($"pack://application:,,,/{Icon}");
+            return new BitmapImage(url);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Mily.Forms/TreeViewTest/Treeview.xaml.cs
+++ Mily.Forms/TreeViewTest/Treeview.xaml.cs
@@ -1,15 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Mily.Forms.TreeViewTest
 {
@@ -25,12 +18,70 @@ namespace Mily.Forms.TreeViewTest
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            foreach (var item in Directory.GetLogicalDrives())
+            //获取本地磁盘
+            foreach (var Drivers in Directory.GetLogicalDrives())
             {
-                var view = new TreeViewItem();
-                view.Header = item;
-                this.views.Items.Add(view);
-            } 
+                var Item = new TreeViewItem()
+                {
+                    //设置头和路径
+                    Header = Drivers,
+                    Tag = Drivers
+                };
+                //添加一个子类
+                Item.Items.Add(null);
+                //添加一个展开的事件
+                Item.Expanded += View_Expanded;
+                //子类添加到主treeview
+                this.views.Items.Add(Item);
+            }
+        }
+
+        private void View_Expanded(object sender, RoutedEventArgs e)
+        {
+            var Item = (TreeViewItem)sender;
+            if (Item.Items[0] != null && Item.Items.Count != 1)
+                return;
+            Item.Items.Clear();
+            var fullpath = (string)Item.Tag;
+            //Dir
+            var Directories = new List<string>();
+            var dirs = Directory.GetDirectories(fullpath);
+            if (dirs.Length > 0)
+                Directories.AddRange(dirs);
+            Directories.ForEach(t =>
+            {
+                var SubItem = new TreeViewItem()
+                {
+                    Header = GetFileOrFolderName(t),
+                    Tag = t
+                };
+                SubItem.Items.Add(null);
+                SubItem.Expanded += View_Expanded;
+                Item.Items.Add(SubItem);
+            });
+            //File
+            var File = new List<string>();
+            var Files = Directory.GetFiles(fullpath);
+            if (Files.Length > 0)
+                File.AddRange(Files);
+            File.ForEach(t =>
+            {
+                var SubItem = new TreeViewItem()
+                {
+                    Header = GetFileOrFolderName(t),
+                    Tag = t
+                };
+                Item.Items.Add(SubItem);
+            });
+        }
+
+        public static string GetFileOrFolderName(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+                return string.Empty;
+            var normal = path.Replace("/", "\\");
+            var last = normal.LastIndexOf("\\");
+            return path.Substring(last + 1);
         }
     }
 }
