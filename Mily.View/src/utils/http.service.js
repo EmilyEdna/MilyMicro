ce7a5e3f7222d28922d048bfd00b358fc2515b03
EmilyEdna <847432003@qq.com>
2020-03-17 21:01:51 +0800

    还原代码

--- Mily.View/src/utils/http.service.js
+++ Mily.View/src/utils/http.service.js
@@ -2,113 +2,12 @@ import axios from 'axios';
 import cookie from 'js-cookie';
 import lzstring from 'lz-string'
 
-let PendingRqesut = [];
-// 默认把请求视为切换路由就会把pending状态的请求取消，false为不取消
-
-//https://juejin.im/post/5d441a98e51d4561db5e39ee
-/**
- * 创建请求
- */
-const service = () => {
-    let Instance = axios.create({
-        baseURL: 'http://127.0.0.1:8520/Proxy/',
-        timeout: 3000,
-        headers: { "Content-Type": "application/json" },
-        withCredentials: false,
-    });
-    // 默认把请求视为切换路由就会把pending状态的请求取消，false为不取消
-    Instance.defaults.routeChangeCancel = true;
-    // 请求拦截
-    instance.interceptors.request.use(ReqeustFilter, error => Promise.reject(error));
-    // 响应拦截
-    instance.interceptors.response.use(response => {
-        ResponseFilter(response);
-        return response.data.Data;
-    }, error => {
-            let ErrorFormat = {};
-            let Response = error.response;
-            // 请求已发出，但服务器响应的状态码不在 2xx 范围内
-            if (Response) {
-                handleResponseIntercept(Response.config);
-                // 设置返回的错误对象格式（按照自己项目实际需求）
-                errorFormat = {
-                    status: response.status,
-                    data: response.data.Data
-                };
-            }
-            // 如果是主动取消了请求，做个标识
-            if (axios.isCancel(error)) {
-                ErrorFormat.SelfCancel = true;
-            }
-            // 其实还有一个情况
-            // 在设置引发错误的请求时，error.message才是错误信息
-            // 但我觉得这个一般是脚本错误，我们项目提示也不应该提示脚本错误给用户看，一般都是我们自定义一些默认错误提示，如“创建成功！”
-            // 所以这里不针对此情况做处理。
-
-            return Promise.reject(errorFormat);
-    });
-    return Instance;
-}
-/**
- * 请求拦截器
- * @param {any} config
- */
-const ReqeustFilter = option => {
-    // 区别请求的唯一标识，这里用方法名+请求路径
-    // 如果一个项目里有多个不同baseURL的请求
-    // 可以改成`${option.method} ${option.baseURL}${option.url}`
-    let RequestMark = `${option.method}[${option.url}]`;
-    // 找当前请求的标识是否存在PendingRqesut中，即是否重复请求了
-    let MarkIndex = PendingRqesut.findIndex(item => {
-        return item.name === RequestMark;
-    });
-    //存在，说明重复了
-    if (MarkIndex > -1) {
-        // 取消上个重复的请求
-        PendingRqesut[MarkIndex].cancel();
-        // 删掉在pendingRequest中的请求标识
-        PendingRqesut.splice(MarkIndex, 1);
-    }
-    // （重新）新建针对这次请求的axios的cancelToken标识
-    const CancelToken = axios.CancelToken;
-    const Source = CancelToken.source();
-    config.cancelToken = Source.token;
-    // 设置自定义配置RequestMark项，主要用于响应拦截中
-    option.Mark = RequestMark;
-    // 记录本次请求的标识
-    PendingRqesut.push({
-        name: RequestMark,
-        cancel: Source.cancel,
-        routeChangeCancel: option.routeChangeCancel // 可能会有优先级高于默认设置的routeChangeCancel项值
-    });
-    let Head = JSON.parse(lzstring.decompressFromBase64(option.headers.Author));
-    if (!Head.Cross) {
-        //如果登录了就把cookie设置到头里
-        option.data.Global = cookie.get("Global");
-        option.headers["Authorization"] = cookie.get("Authorization");
-    }
-    option.data = JSON.stringify(option.data);
-    return option;
-}
-/**
- * 响应拦截器
- * @param {any} option
- */
-const ResponseFilter = setting => {
-    // 根据请求拦截里设置的RequestMark配置来寻找对应PendingRqesut里对应的请求标识
-    let MarkIndex = PendingRqesut.findIndex(item => {
-        return item.name === setting.config.Mark;
-    });
-    // 找到了就删除该标识
-    if (MarkIndex > -1)
-        pendingRequest.splice(MarkIndex, 1);
-    let Head = JSON.parse(lzstring.decompressFromBase64(setting.option.headers.Author));
-    if (Head.Cross) {
-        cookie.set("Global", lzstring.compressToBase64(setting.data.Data.ResultData.Data.KeyId));
-        cookie.set("Authorization", setting.data.Data.ResultData.AuthorToken);
-    }
-}
-
+const service = axios.create({
+    baseURL: 'http://127.0.0.1:8520/Proxy/',
+    timeout: 3000,
+    headers: { "Content-Type": "application/json" },
+    withCredentials: false,
+});
 
 /*
  *处理请求
