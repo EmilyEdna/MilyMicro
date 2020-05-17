08ade04d747d45da292d8a7f93bc8ca7115e35d5
EmilyEdna <847432003@qq.com>
2020-05-17 12:28:43 +0800

    测试

---
+++ Miliy.MainApi/Controllers/SocketTestController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mily.Socket.SocketCall;
+using Mily.Socket.SocketConfig;
+using Mily.Socket.SocketEvent;
+
+namespace Miliy.MainApi.Controllers
+{
+    [Route("Api/[controller]/[action]")]
+    [ApiController]
+    public class SocketTestController : Controller
+    {
+        [HttpGet]
+        public async Task<ActionResult<string>> SocketTest() 
+        {
+            SocketSerializeData SSD = new SocketSerializeData(); ;
+            SSD.AppendRoute("TestApi/other/GetTest");
+            SSD.AppendSerialized("Name", "lzh");
+            Call.SendInternalInfo(SSD);
+            var data = CallEventAction.Instance().DelegateResult;
+            return await Task.FromResult("123");
+        }
+    }
+}
--- Mily.OtherApi/Controllers/OtherController.cs
+++ Mily.OtherApi/Controllers/OtherController.cs
@@ -6,12 +6,21 @@ using System;
 using Mily.Socket.SocketConfig;
 using Mily.Socket.SocketCall;
 using System.Threading;
+using XExten.Common;
 
 namespace Mily.OtherApi.Controllers
 {
     [Route("Api/[controller]/[action]")]
     [ApiController]
+    [SocketRoute("TestApi")]
     public class OtherController : Controller
     {
+        [SocketMethod]
+        [SocketAuthor(false)]
+        public async Task<ActionResult<Object>> GetTest(ResultProvider Param)
+        {
+            return await Task.FromResult("我是测测试");
+        }
+
     }
 }
