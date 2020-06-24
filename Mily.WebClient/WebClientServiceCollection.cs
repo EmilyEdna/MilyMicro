a77febdf22e3674a524dbc43d0a0f492ffaba877
EmilyEdna <847432003@qq.com>
2020-06-24 17:50:56 +0800

    补齐内容

--- Mily.WebClient/WebClientServiceCollection.cs
+++ Mily.WebClient/WebClientServiceCollection.cs
@@ -1,17 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Microsoft.Extensions.DependencyInjection;
-using XExten.XPlus
+using WebApiClientCore;
+using XExten.XPlus;
 
 namespace Mily.WebClient
 {
     public static class WebClientServiceCollection
     {
-
-        public static IServiceCollection AddClientApi(this IServiceCollection Services)
-        { 
-        
+        /// <summary>
+        /// 注册ClientApi
+        /// </summary>
+        /// <param name="Services"></param>
+        /// <returns></returns>
+        public static IServiceCollection RegiestClientApi(this IServiceCollection Services)
+        {
+            var Assemlies = XPlusEx.XAssembly();
+            var Types = Assemlies.SelectMany(t=>t.ExportedTypes.Where(x => x.GetInterfaces().Contains(typeof(IHttpApi))));
+            foreach (var Item in Types)
+            {
+                Services.AddHttpApi(Item, opt =>
+                {
+                    opt.JsonSerializeOptions.PropertyNamingPolicy = null;
+                    opt.JsonSerializeOptions.DictionaryKeyPolicy = null;
+                    opt.UseParameterPropertyValidate = false;
+                    opt.UseReturnValuePropertyValidate = false;
+                });
+            }
+            return Services;
         }
     }
 }
